127d68f965713174dcd1093fe5c110c65b40b4ad
nxa1986 <van.nguyen@texo.design>
2019-07-08 18:13:37 +0700

    inital form

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,17 @@
+//  Fixed Nav =====================
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("menuHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("stickyNav");
+  } else {
+    header.classList.remove("stickyNav");
+  }
+}
+
 //  Slider =====================
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -12,7 +26,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("bannerSlider");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
@@ -20,8 +34,8 @@ function showSlides(n) {
       slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" activeDot", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " activeDot";
 }
