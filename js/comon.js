07b79e59d4f455a6c581c8dd817fdf24bb1aaef6
nxa1986 <anh.nguyen@texo.design>
2019-07-17 18:12:35 +0700

    inital about

--- js/comon.js
+++ js/comon.js
@@ -39,3 +39,28 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " activeDot";
 }
+
+//  datePicker =====================
+$(document).ready(function(){
+  $("#checkin").datepicker({
+    dateFormat:"dd MM yy",
+    showOn: "button",
+    buttonImage: "image/sort-icon.png",
+    buttonImageOnly: true,
+  })
+  .datepicker("setDate", new Date());
+  $( ".ui-datepicker-trigger" ).css("cursor","pointer");
+  $(".ui-datepicker-trigger").css("margin-top","1.7em");
+});
+
+$(document).ready(function(){
+  $("#checkout").datepicker({
+    dateFormat:"dd MM yy",
+    showOn: "button",
+    buttonImage: "image/sort-icon.png",
+    buttonImageOnly: true,
+  })
+  .datepicker("setDate", new Date());
+  $( ".ui-datepicker-trigger" ).css("cursor","pointer");
+  $(".ui-datepicker-trigger").css("margin-top","1.7em");
+});
